0ae875f916f4947a6d58e6f41603aef304c4eb98
Volodymyr <vovaneko18@gmail.com>
2025-07-21 13:48:16 +0300

    Some changes in StopWatch and Clock
    Using Diagnostics.Stopwatch and Removing Thread in Clock And Stopwatch

--- Clock/Clock/Program.cs
+++ Clock/Clock/Program.cs
@@ -13,49 +13,50 @@ namespace Clock
 {
     public class Start {
         public void Starting() {
+            string choice = "";
             while (true)
             {
                 Console.WriteLine("1 - Clock\n2 - Stopwatch\n3 - Timer\nSpacebar - exit");
-                char choise = '-';
                 var key = Console.ReadKey(true);
                 if (key.Key == ConsoleKey.D1)
                 {
-                    choise = '1';
+                    choice = "1";
                 }
                 else if (key.Key == ConsoleKey.D2)
                 {
-                    choise = '2';
+                    choice = "2";
                 }
                 else if (key.Key == ConsoleKey.D3)
                 {
-                    choise = '3';
+                    choice = "3";
                 }
                 else if (key.Key == ConsoleKey.Spacebar)
                 {
-                    choise = '4';
+                    choice = "4";
                 }
-                switch (choise)
+                while (Console.KeyAvailable)
                 {
-                    case '1':
+                    Console.ReadKey(true);
+                }
+                switch (choice)
+                {
+                    case "1":
                         Console.Clear();
                         Clock clock = new Clock();
                         clock.Start();
                         break;
-                    case '2':
+                    case "2":
                         Console.Clear();
                         Stopwatch stopwatch = new Stopwatch();
                         stopwatch.Start();
                         break;
-                    case '3':
+                    case "3":
                         Console.Clear();
                         Timer timer = new Timer();
                         timer.Start();
                         break;
-                    case '4':
+                    case "4":
                         Environment.Exit(0);
-                        break;
-                    case ' ':
-                        Console.Clear();
                         break;
                     default:
                         Console.Clear();
@@ -64,56 +65,100 @@ namespace Clock
             }
         }
     }
-    public class Clock {
-        DateTime DT;
-        private static bool IsExists = true;
-        public void Start() {
-            while (IsExists)
+    public class Clock
+    {
+        private bool _isRunning = true;
+
+        public void Start()
+        {
+            Console.CursorVisible = false;
+
+            while (_isRunning)
             {
-                Thread thread = new Thread(InThread);
-                thread.Start();
-                DT = DateTime.Now;
-                Console.WriteLine($"{DT.Hour}:{DT.Minute}:{DT.Second}");
-                Thread.Sleep(1000);
                 Console.Clear();
+                Console.WriteLine("Escape to Exit");
+
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine($"{currentTime.Hour:D2}:{currentTime.Minute:D2}:{currentTime.Second:D2}");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        _isRunning = false;
+                    }
+                    while (Console.KeyAvailable)
+                    {
+                        Console.ReadKey(true);
+                    }
+                }
+                Thread.Sleep(100); 
+   
             }
-        }
-        public void InThread() {
-            var key = Console.ReadKey(true);
-            if (key.Key == ConsoleKey.Escape)
-            {
-                IsExists = false;
-                return;
-            }
-            return;
+
+            Console.Clear();
+            Console.WriteLine("Clock stopped. Returning to main menu...");
+            Console.CursorVisible = true;
         }
     }
     public class Stopwatch
     {
-        DateTime DT;
-        private static bool IsExists = true;
-        public Stopwatch() { DT = DateTime.Today; }
+        private bool _isRunning = true;
+        private System.Diagnostics.Stopwatch _internalStopwatch;
         public void Start()
         {
-            while (IsExists)
+            Console.CursorVisible = false;
+
+            _internalStopwatch = new System.Diagnostics.Stopwatch();
+            _internalStopwatch.Start();
+
+            Console.Clear();
+            while (_isRunning)
             {
-                Thread thread = new Thread(InThread);
-                thread.Start();
-                Console.WriteLine($"{DT.Hour}:{DT.Minute}:{DT.Second}");
-                DT = DT.AddSeconds(1);
-                Thread.Sleep(1000);
                 Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Escape to Exit (Spacebar to Toggle Pause)");
+
+                TimeSpan elapsed = _internalStopwatch.Elapsed;
+
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine($"Elapsed: {elapsed.Hours:D2}:{elapsed.Minutes:D2}:{elapsed.Seconds:D2}.{elapsed.Milliseconds:D3}");
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        _isRunning = false;
+                    }
+                    else if (key.Key == ConsoleKey.Spacebar) {
+                        if (_internalStopwatch.IsRunning)
+                        {
+                            _internalStopwatch.Stop();
+                            Console.SetCursorPosition(0, 2);
+                            Console.WriteLine("Paused. Press Spacebar to resume");
+                        }
+                        else
+                        {
+                            _internalStopwatch.Start();
+                            Console.SetCursorPosition(0, 2);
+                            Console.WriteLine("                                ");
+                        }
+                    }
+                    while (Console.KeyAvailable)
+                        {
+                            Console.ReadKey(true);
+                        }
+                }
+                Thread.Sleep(50);
             }
-        }
-        public void InThread()
-        {
-            var key = Console.ReadKey(true);
-            if (key.Key == ConsoleKey.Escape)
-            {
-                IsExists = false;
-                return;
-            }
-            return;
+            _internalStopwatch.Stop();
+            Console.Clear();
+            Console.WriteLine($"Final Elapsed Time: {_internalStopwatch.Elapsed.Hours:D2}:{_internalStopwatch.Elapsed.Minutes:D2}:{_internalStopwatch.Elapsed.Seconds:D2}.{_internalStopwatch.Elapsed.Milliseconds:D3}");
+            Console.WriteLine("Clock stopped. Returning to main menu...");
+            Console.CursorVisible = true;
         }
     }
     public class Timer {
